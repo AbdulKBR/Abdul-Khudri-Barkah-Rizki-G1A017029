318cc760516d82adb0d72e9cd6dc1547ee651a8a
Abdul Khudri Barkah Rizki <99132609+AbdulKBR@users.noreply.github.com>
2022-02-25 23:59:51 +0700

    Create pengukur_waktu_tidur.java

---
+++ pengukur_waktu_tidur.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class javaCalculator 
+{
+    public static void main(String[] args) 
+    {
+        int waktu;
+        Scanner input = new Scanner(System.in);
+        System.out.println("tolong masukkan berapa jam anda tidur sehari:");
+        waktu = input.nextInt();
+        if (waktu >=7 && waktu <= 9){
+            System.out.println("normal");
+        }else if(waktu > 9){
+            System.out.println("berlebihan");
+        }else if(waktu < 7){
+            System.out.println("kurang");
+        }
+    }
+}
